da35d6d0fb16d96c0ede05a266857232b031ae47
Inferis <spam@inferis.org>
2012-02-07 23:33:01 +0100

    Added Add Account controller.

---
+++ Faering/UI/Shared/AddAccountController.h
@@ -0,0 +1,13 @@
+//
+//  AddAccountController.h
+//  Faering
+//
+//  Created by Tom Adriaenssen on 07/02/12.
+//  Copyright (c) 2012 Adriaenssen BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddAccountController : UIViewController
+
+@end
