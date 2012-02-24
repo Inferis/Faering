7972af5e6fc4bba7935dc51dea7c0eeae7b3f8f2
Inferis <spam@inferis.org>
2012-02-24 14:33:15 +0100

    Reworked overview.

--- Faering/UI/iPhone/SimOverviewPhoneController.h
+++ Faering/UI/iPhone/SimOverviewPhoneController.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "HasSim.h"
 
-@interface SimOverviewPhoneController : UITableViewController<HasSim>
+@interface SimOverviewPhoneController : UIViewController<HasSim>
 
 @end
